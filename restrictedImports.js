228af1deb78d4161849fba4b48e3583554c85a98
Mike <grabbou@gmail.com>
2022-12-13 12:28:45 -0500

    chore: update some rules

--- restrictedImports.js
+++ restrictedImports.js
@@ -1,11 +1,6 @@
 exports.node = {
   paths: [
     {
-      name: '@ethersproject/providers',
-      message: 'Please only use Providers instantiated in constants/providers to improve traceability.',
-      allowTypeImports: true,
-    },
-    {
       name: 'ethers',
       message: "Please import from '@ethersproject/module' directly to support tree-shaking.",
     },
@@ -24,11 +19,6 @@ exports.react = {
     {
       name: 'styled-components',
       message: 'Please import from styled-components/macro.',
-    },
-    {
-      name: '@lingui/macro',
-      importNames: ['t'],
-      message: 'Please use <Trans> instead of t.',
     },
   ],
   patterns: exports.node.patterns,
