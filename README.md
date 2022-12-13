15b6428ecfe51339c3832ebd76d8d5448afbe7aa
Mike Grabowski <grabbou@gmail.com>
2022-12-13 12:39:12 -0500

    feat: ESLint configuration for React / Node with Prettier support  (#2)
    * chore: let's go
    
    chore: new line at the end
    
    chore: add missing dep
    
    chore: update deps to latest
    
    chore: one more time
    
    feat: add readme + example
    
    chore: some readme updates
    
    * chore: typo
    
    * chore: refactor
    
    * chore: bring back original values
    
    * chore: add tests
    
    * chore: fix peers
    
    * chore: add files and update decs
    
    * chore: rename
    
    * chore: update'
    
    * chore: tweaks
    
    * chore: add object shorthand
    
    * chore: tweaks
    
    * chore: update envs
    
    * chore: update some rules
    
    * chore: readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# @uniswap/eslint-config
+
+Uniswap ESLint config to enforce coding standards and best practices.
+
+- TypeScript and Jest support
+- Seamless Prettier integration
+- Separate environments for Node.js and React
+
+## Installation
+
+```bash
+yarn add --dev eslint @uniswap/eslint-config
+```
+
+## Usage
+
+```json
+{
+  "extends": "@uniswap"
+}
+```
+
+> This will extend ESLint config for Node.js 
+
+or:
+
+```json
+{
+  "extends": "@uniswap/eslint-config/react"
+}
+```
+
+#### VSCode
+
+If you're a VSCode user, consider installing the official [ESLint plugin](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint) and copying [`./.vscode/settings.json`](.vscode/settings.json#L1-L6) to your own `.vscode/settings.json` configuration file. This will automatically run `eslint --fix` on save.
---
+++ node.js
@@ -0,0 +1,64 @@
+const { node: nodeRestrictedImports } = require('./restrictedImports')
+
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ['prettier', 'import', 'simple-import-sort', 'unused-imports'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        printWidth: 120,
+      },
+    ],
+    'import/no-unused-modules': ['error', { unusedExports: true }],
+    'object-shorthand': ['error', 'always'],
+    'unused-imports/no-unused-imports': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+          typescript: {
+            alwaysTryTypes: true,
+          },
+        },
+      },
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-restricted-imports': ['error', nodeRestrictedImports],
+      },
+    },
+    {
+      // Taken from Jest's default 'testMatch' config
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+      env: {
+        jest: true,
+        'jest/globals': true,
+      },
+      extends: ['plugin:jest/recommended'],
+      plugins: ['jest'],
+    },
+  ],
+}
