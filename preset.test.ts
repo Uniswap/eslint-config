c5d3bcdfa96896ae865b0f9d7c673c42939700f8
Mike <grabbou@gmail.com>
2022-12-16 20:47:43 -0500

    feat: disable jest for cypress and enable dedicated plugin

--- preset.test.ts
+++ preset.test.ts
@@ -6,7 +6,7 @@ it('should have a correct configuration for a TypeScript file', async () => {
       extends: ['./node.js'],
     },
   })
-  expect(await linter.calculateConfigForFile('node.test.ts')).toMatchSnapshot({ parser: expect.any(String) })
+  expect(await linter.calculateConfigForFile('node.ts')).toMatchSnapshot({ parser: expect.any(String) })
 })
 
 it('should have a correct configuration for a React file', async () => {
@@ -16,4 +16,26 @@ it('should have a correct configuration for a React file', async () => {
     },
   })
   expect(await linter.calculateConfigForFile('node.tsx')).toMatchSnapshot({ parser: expect.any(String) })
+})
+
+it('should have a correct configuration for a Jest file', async () => {
+  const linter = new ESLint({
+    overrideConfig: {
+      extends: ['./node.js'],
+    },
+  })
+  expect(await linter.calculateConfigForFile('src/feature/node.test.ts')).toMatchSnapshot({
+    parser: expect.any(String),
+  })
+})
+
+it('should have a correct configuration for a Cypress e2e file', async () => {
+  const linter = new ESLint({
+    overrideConfig: {
+      extends: ['./node.js'],
+    },
+  })
+  expect(await linter.calculateConfigForFile('cypress/e2e/node.test.ts')).toMatchSnapshot({
+    parser: expect.any(String),
+  })
 })
